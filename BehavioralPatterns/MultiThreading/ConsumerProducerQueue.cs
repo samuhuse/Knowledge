b92f6e2f3159921aad6dcd52333935d327b6ea9a
samuele lombardi <samuele.mn@hotmail.it>
2021-05-15 19:13:24 +0200

    Updated Consumer Producer Pattern

--- BehavioralPatterns/MultiThreading/ConsumerProducerQueue.cs
+++ BehavioralPatterns/MultiThreading/ConsumerProducerQueue.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,9 @@ using System.Threading.Tasks;
 
 namespace BehavioralPatterns.MultiThreading
 {
+    #region Explained
     public class ConsumerProducerQueue
     {
-        Queue<Action> Queue = new Queue<Action>();
-        CancellationTokenSource CancellationSource = new CancellationTokenSource();
-
         public class Producer 
         {
             private readonly Queue<Action> _queue;
@@ -29,7 +28,6 @@ namespace BehavioralPatterns.MultiThreading
             {
                 return new Task(() =>
                 {
-                    int c = 0;
                     while (true)
                     {
                         Console.WriteLine("Producing " + Task.CurrentId);
@@ -58,7 +56,6 @@ namespace BehavioralPatterns.MultiThreading
             {
                 return new Task(() =>
                 {
-                    Console.WriteLine(Task.CurrentId);
                     while (true)
                     {
                         Action action = null;
@@ -77,10 +74,13 @@ namespace BehavioralPatterns.MultiThreading
             }
         }
 
+        Queue<Action> Queue = new Queue<Action>();
+        CancellationTokenSource CancellationSource = new CancellationTokenSource();
+
         [Test]
         public void Try()
         {
-            Action action = () => Console.WriteLine($"Writed by {Thread.CurrentThread.ManagedThreadId}");
+            Action action = () => Console.WriteLine($"Consumed by {Thread.CurrentThread.ManagedThreadId}");
 
             new Producer(Queue).Produce(action, CancellationSource.Token).Start();
 
@@ -99,4 +99,93 @@ namespace BehavioralPatterns.MultiThreading
         }
 
     }
+    #endregion
+
+    #region UseCase
+
+    public class ConsumerproducerCuncurrent
+    {
+
+        public class Producer
+        {
+            private readonly IProducerConsumerCollection<Action> _collection;
+            private readonly Random _random;
+
+            public Producer(IProducerConsumerCollection<Action> collection)
+            {
+                _collection = collection;
+                _random = new Random();
+            }
+
+            public Task Produce(Action action, CancellationToken token)
+            {
+                return new Task(() =>
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Producing " + Task.CurrentId);
+
+                        _collection.TryAdd(action);                        
+
+                        Task.Delay(_random.Next(1, 2) * 1000).Wait();
+                        token.ThrowIfCancellationRequested();
+                    }
+                });
+            }
+        }
+
+        public class Consumer
+        {
+            private readonly IProducerConsumerCollection<Action> _collection;
+
+            public Consumer(IProducerConsumerCollection<Action> collection)
+            {
+                _collection = collection;
+            }
+
+            public Task Work(CancellationToken token)
+            {
+                return new Task(() =>
+                {
+                    while (true)
+                    {
+                        Action action = null;
+
+                        _collection.TryTake(out action);
+
+                        action?.Invoke();
+
+                        token.ThrowIfCancellationRequested();
+                    }
+                });
+            }
+        }
+
+        IProducerConsumerCollection<Action> Colection = new ConcurrentBag<Action>();
+        CancellationTokenSource CancellationSource = new CancellationTokenSource();
+
+        [Test]
+        public void Try()
+        {
+            Action action = () => Console.WriteLine($"Consumed by {Thread.CurrentThread.ManagedThreadId}");
+
+            new Producer(Colection).Produce(action, CancellationSource.Token).Start();
+
+            List<Task> consumerlist = new List<Task>()
+            {
+                new Consumer(Colection).Work(CancellationSource.Token),
+                new Consumer(Colection).Work(CancellationSource.Token),
+                new Consumer(Colection).Work(CancellationSource.Token),
+                new Consumer(Colection).Work(CancellationSource.Token)
+            };
+            consumerlist.ForEach(t => t.Start());
+
+            Task.Delay(10 * 1000).Wait();
+
+            CancellationSource.Cancel();
+        }
+
+    }
+
+    #endregion
 }
