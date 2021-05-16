e29ecaa4578032852d973ac93295b942513c3482
samuele lombardi <samuele.mn@hotmail.it>
2021-05-16 17:31:09 +0200

    Added Linq

---
+++ BehavioralFeatures/Linq/Advanced.cs
@@ -0,0 +1,255 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehavioralFeatures.Linq
+{
+    public class Advanced
+    {
+        #region Model
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Id { get; set; }
+            public int Height { get; set; }
+            public int Age { get; set; }
+
+            public Gender Gender;
+
+            public Person(string firstName, string lastName, int id, int height, int age, Gender gender)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Id = id;
+                Height = height;
+                Gender = gender;
+                Age = age;
+            }
+        }
+
+        internal class Supplier
+        {
+            public string Name { get; set; }
+            public string District { get; set; }
+            public int Age { get; set; }
+        }
+
+        internal class Buyer
+        {
+            public string Name { get; set; }
+            public string District { get; set; }
+            public int Age { get; set; }
+        }
+
+        #endregion
+
+        List<Person> people;
+
+        List<Buyer> buyers;
+        List<Supplier> suppliers;
+
+        void PrintEnumerable<T>(IEnumerable<IGrouping<T, Person>> groups)
+        {
+            foreach (IGrouping<T, Person> item in groups)
+            {
+                Console.WriteLine($"{item.Key}:");
+                foreach (var p in item)
+                {
+                    Console.WriteLine($" {p.FirstName}");
+                }
+            }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            people = new List<Person>()
+            {
+                new Person("Tod", "Vachev", 1, 180, 26, Gender.Male),
+                new Person("John", "Johnson", 2, 170, 21, Gender.Male),
+                new Person("Anna", "Maria", 3, 150, 22, Gender.Female),
+                new Person("Kyle", "Wilson", 4, 164, 29, Gender.Male),
+                new Person("Anna", "Williams", 5, 164, 28, Gender.Male),
+                new Person("Maria", "Ann", 6, 160, 19, Gender.Female),
+                new Person("John", "Jones", 7, 160, 22, Gender.Female),
+                new Person("Samba", "TheLion", 8, 175, 23, Gender.Male),
+                new Person("Aaron", "Myers", 9, 182, 21, Gender.Male),
+                new Person("Aby", "Wood", 10, 165, 20, Gender.Female),
+                new Person("Maddie","Lewis",  11, 160, 19, Gender.Female),
+                new Person("Lara", "Croft", 12, 162, 23, Gender.Female)
+            };
+
+            buyers = new List<Buyer>()
+            {
+                new Buyer() { Name = "Johny", District = "Fantasy District", Age = 22},
+                new Buyer() { Name = "Peter", District = "Scientists District", Age = 40},
+                new Buyer() { Name = "Paul", District = "Fantasy District", Age = 30 },
+                new Buyer() { Name = "Maria", District = "Scientists District", Age = 35 },
+                new Buyer() { Name = "Joshua", District = "EarthIsFlat District", Age = 40 },
+                new Buyer() { Name = "Sylvia", District = "Developers District", Age = 22 },
+                new Buyer() { Name = "Rebecca", District = "Scientists District", Age = 30 },
+                new Buyer() { Name = "Jaime", District = "Developers District", Age = 35 },
+                new Buyer() { Name = "Pierce", District = "Fantasy District", Age = 40 }
+            };
+
+            suppliers = new List<Supplier>()
+            {
+                new Supplier() { Name = "Harrison", District = "Fantasy District", Age = 22 },
+                new Supplier() { Name = "Charles", District = "Developers District", Age = 40 },
+                new Supplier() { Name = "Hailee", District = "Scientists District", Age = 35 },
+                new Supplier() { Name = "Taylor", District = "EarthIsFlat District", Age = 30 }
+            };
+        }
+
+        record yearAge(Gender gender, int age);
+
+        [Test]
+        public void Grouping()
+        {
+            IEnumerable<IGrouping<Gender, Person>> genderGroup = people.GroupBy(p => p.Gender);
+            PrintEnumerable(genderGroup);
+
+            IEnumerable<IGrouping<char, Person>> alphabeticalGroup = people.OrderBy(p => p.FirstName).GroupBy(p => p.FirstName[0]);
+            PrintEnumerable(alphabeticalGroup);
+
+            IEnumerable<IGrouping<yearAge, Person>> multiGroup = people.GroupBy(p => new yearAge(p.Gender, p.Age));
+            PrintEnumerable(multiGroup);
+
+            IEnumerable<IGrouping<yearAge, Person>> multiGroupOrdered = multiGroup.OrderBy(g => g.Count());
+            PrintEnumerable(multiGroupOrdered);
+
+            IEnumerable<IGrouping<int, Person>> GroupOrderedByKey = people.GroupBy(p => p.Age).OrderBy(g => g.Key);
+            PrintEnumerable(multiGroupOrdered);
+
+            IEnumerable<IGrouping<string, Person>> GroupByCondition = people.GroupBy(p => (p.Age % 2 == 0) ? "Even" : "Odd");
+            PrintEnumerable(multiGroupOrdered);
+
+            var GroupCount = people.GroupBy(p => p.Age).Select(g => new { Age = g.Key, Count = g.Count() });
+            foreach (var amount in GroupCount)
+            {
+                Console.WriteLine($"{amount.Age}");
+                Console.WriteLine($"{amount.Count}");
+            }
+        }
+
+        [Test]
+        public void Join()
+        {
+            var innerJoin = buyers.Join(suppliers,
+                                        s => s.District,
+                                        b => b.District,
+                                        (b, s) => new { District = s.District, Supplier = s, Buyer = b });
+
+            foreach (var item in innerJoin)
+            {
+                Console.WriteLine($"District: {item.District}, Supplier: {item.Supplier}, Buyer: {item.Buyer}");
+            }
+
+            var compositeInnerJoin = buyers.Join(suppliers,
+                                                 s => new { s.District, s.Age },
+                                                 b => new { b.District, b.Age },
+                                                 (b, s) => new { Supplier = s, Buyer = b });
+
+            foreach (var item in innerJoin)
+            {
+                Console.WriteLine($"Supplier: {item.Supplier}, Buyer: {item.Buyer}, Age:{item.Supplier.Age}");
+            }
+        }
+
+        [Test]
+        public void GroupJoin()
+        {
+            var matchingSuppliersWithBuyers = suppliers.GroupJoin(
+                                          buyers,
+                                          s => s.District,
+                                          b => b.District,
+                                          (s, buyersGroup) =>
+                                             new
+                                             {
+                                                 Name = s.Name,
+                                                 District = s.District,
+                                                 Buyers = buyersGroup.OrderBy(b => b.Age)
+                                             }
+                                       );
+
+            foreach (var item in matchingSuppliersWithBuyers)
+            {
+                Console.WriteLine($"Supplier: {item.Name}, District: {item.District} " +
+                    $"\nBuyers:");
+
+                foreach (var buyer in item.Buyers)
+                {
+                    Console.WriteLine($"  {buyer.Name}");
+                }
+            }
+        }
+
+        [Test]
+        public void LeftOuterJoin()
+        {
+            var leftOuterJoinType = suppliers.GroupJoin(buyers,
+                                                        s => s.District,
+                                                        b => b.District,
+                                                        (s, buyersGroup) =>
+                                                            new
+                                                            {
+                                                                Name = s.Name,
+                                                                District = s.District,
+                                                                Buyers = buyersGroup.DefaultIfEmpty(new Buyer() { Name = "No name", District = "No place" })
+                                                            }
+                                                       );
+
+
+            foreach (var item in leftOuterJoinType)
+            {
+                Console.WriteLine($"Supplier: {item.Name}, District: {item.District} " +
+                    $"\nBuyers:");
+
+                foreach (var buyer in item.Buyers)
+                {
+                    Console.WriteLine($"  {buyer.Name}");
+                }
+            }
+
+            var leftOuterJoinAnon = suppliers.GroupJoin(buyers,
+                                                        s => s.District,
+                                                        b => b.District,
+                                                        (s, buyersGroup) =>
+                                                            new
+                                                            {
+                                                                s = s,
+                                                                buyersGroup = buyersGroup
+                                                            }
+                                                        )
+                                                        .SelectMany(
+                                                            s => s.buyersGroup.DefaultIfEmpty(),
+                                                            (s, b) =>
+                                                                new
+                                                                {
+                                                                    Name = s.s.Name,
+                                                                    District = s.s.District,
+                                                                    BuyersName = (b.Name ?? "No one here"),
+                                                                    BuyersDistrict = (b.District ?? "Nowhere")
+                                                                }
+                                                        );
+
+            foreach (var item in leftOuterJoinAnon)
+            {
+                Console.WriteLine($"{item.District}");
+                Console.WriteLine($"  {item.Name}, {item.BuyersName}");
+            }
+        }
+    }
+}
---
+++ BehavioralFeatures/Linq/Basic.cs
@@ -0,0 +1,206 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehavioralFeatures.Linq
+{
+    public class Basic
+    {
+        #region Model
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Id { get; set; }
+            public int Height { get; set; }
+            public int Age { get; set; }
+
+            public Gender Gender { get; set; }
+        }
+
+        internal class Buyer : Person
+        {
+            public int Buget { get; set; }
+        }
+
+        internal class Supplier : Person
+        {
+            public string Product { get; set; }
+        }
+
+        #endregion
+
+        List<Person> people;
+
+        void Print(IEnumerable<Person> people)
+        {
+            people.ToList().ForEach(p => Console.WriteLine($"Name: {p.FirstName}, Surname: {p.LastName}, Age: {p.Age}"));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            people = new List<Person>()
+            {
+                new Person{FirstName="Tod", LastName="Vachev", Id=1, Height=180, Age = 26, Gender =Gender.Male },
+                new Person{FirstName="John", LastName="Johnson",Id= 2, Height=170, Age = 21,Gender = Gender.Male},
+                new Person{FirstName="Anna", LastName="Maria",Id= 3, Height=150,Age =  22,Gender = Gender.Female},
+                new Person{FirstName="Kyle", LastName="Wilson",Id= 4,Height= 164, Age = 29,Gender = Gender.Male},
+                new Person{FirstName="Anna", LastName="Williams",Id= 5,Height= 164, Age = 28,Gender = Gender.Male},
+                new Person{FirstName="Maria",LastName= "Ann", Id=6, Height=160,Age =  19, Gender =Gender.Female},
+                new Person{FirstName="John", LastName="Jones",Id= 7,Height= 160,Age =  22,Gender = Gender.Female},
+                new Person{FirstName="Samba", LastName="TheLion",Id= 8,Height= 175, Age = 23,Gender = Gender.Male},
+                new Person{FirstName="Aaron", LastName="Myers",Id= 9, Height=182, Age = 21, Gender =Gender.Male},
+                new Person{FirstName="Aby", LastName="Wood",Id= 10, Height=165, Age = 20, Gender =Gender.Female},
+                new Supplier{FirstName="Maddie",LastName="Lewis",Id=  11,Height= 160,Age =  19, Gender =Gender.Female, Product="Tea"},
+                new Buyer{FirstName="Lara",LastName= "Croft", Id=12, Height=162, Age = 23, Gender =Gender.Female, Buget= 100}
+            };
+        }
+
+        [Test]
+        public void Where()
+        {
+            IEnumerable<Person> result = people.Where(p => p.Age > 21);
+            Print(result);
+        }
+
+        [Test]
+        public void Select()
+        {
+            IEnumerable<int> result = people.Select(p => p.Age);
+            result.ToList().ForEach(a => Console.WriteLine($"Age: {a}"));
+        }
+
+        [Test]
+        public void Orderings()
+        {
+            IEnumerable<Person> result = people.OrderBy(p => p.Age).ThenByDescending(p=> p.FirstName);
+            Print(result);
+        }
+
+        [Test]
+        public void Aggregations()
+        {
+            int ageMax = people.Max(p => p.Age);
+            Console.WriteLine(ageMax);
+
+            int ageMin = people.Min(p => p.Age);
+            Console.WriteLine(ageMin);
+
+            int ageSum = people.Sum(p => p.Age);
+            Console.WriteLine(ageSum);
+
+            double ageAverage = people.Average(p => p.Age);
+            Console.WriteLine(ageAverage);
+
+            long customAggregation = people.Aggregate(seed:1, (x, p) => p.Age * x);
+            Console.WriteLine(customAggregation);
+        }
+
+        [Test]
+        public void Qualifiers()
+        {
+            bool atLeastOne = people.Any(p=> p.Age == 21);
+            Console.WriteLine(atLeastOne);
+
+            bool ifAlls = people.All(p => p.Age > 21);
+            Console.WriteLine(ifAlls);
+
+            bool containsReference = people.Contains(people.FirstOrDefault());
+            Console.WriteLine(containsReference);
+
+        }
+
+        [Test]
+        public void EnumerableComparison()
+        {
+            string[] catNames = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Toby", "Loki", "Oscar" };
+            string[] catNames2 = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Toby", "Loki", "Oscar" };
+
+            Console.WriteLine(catNames == catNames2);             // False
+            Console.WriteLine(Equals(catNames, catNames2));       // False
+            Console.WriteLine(catNames.Equals(catNames2));        // False
+            Console.WriteLine(catNames.SequenceEqual(catNames2)); // True
+        }
+
+        [Test]
+        public void SetOperations()
+        {
+            void Print<T>(IEnumerable<T> set)
+            {
+                set.ToList().ForEach(i => Console.WriteLine(i));
+            }
+
+            string st1 = "I am a cat";
+            string st2 = "I am a dog";
+            List<int> ints = new List<int>() { 1, 2, 2, 2, 3, 3, 4, 5, 6, 5, 6, 5, 6, 5, 3, 4, 5, 6, 7, 8, 8, 4, 3 };
+            List<int> ints2 = new List<int>() { 3, 2, 3, 5, 8, 43, 5, 67, 1, 2, 3, 7, 7, 7, 6, 5, 2, 1, 1, 1, 1, 1 };
+
+            var distinct = st1.Distinct(); // gets all unique items, disregarding their repetitions
+            Print(st1);
+            var distinct2 = st2.Distinct();
+            Print(st2);
+
+            var intDistinct = ints.Distinct();
+            Print(intDistinct);
+
+            var intersect = st1.Intersect(st2); // gets all matching unique items between two collections
+            Print(intersect);
+            var intIntersect = ints.Intersect(ints2);
+            Print(intIntersect);
+
+            var union = st1.Union(st2); // gets all unique items from both collections that are no repeating, like two distincts
+            Print(union);
+            var union2 = st2.Union(st1); // same result, just in different order
+            Print(union2);
+            var intUnion = ints.Union(ints2);
+            Print(intUnion);
+
+            var except = st1.Except(st2); // gets all items from st1 that are not present in st2
+            Print(except);
+            var except2 = st2.Except(st1); // gets all items from st2 that are not present in st1
+            Print(except2);
+
+            var intExcept = ints.Except(ints2);
+            Print(intExcept);
+            var intExcept2 = ints2.Except(ints);
+            Print(intExcept2);
+        }
+
+        [Test]
+        public void Conversions()
+        {
+            IEnumerable<Person> result = people.OfType<Buyer>() // Filter by type
+                                               .ToList()
+                                               .ConvertAll(b => new Supplier() { Product = "Software" });
+            Print(result);
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            List<string> stringNumbers = numbers.ConvertAll(n => n.ToString());
+            stringNumbers.ForEach(n => Console.WriteLine(n));
+        }
+
+        [Test]
+        public void Partitioning()
+        {
+            var result = people.OrderBy(p => p.Age).TakeWhile(p => p.Age < 25).Take(2);
+            Print(result);
+
+            result = people.OrderBy(p => p.Age).SkipWhile(p => p.Age < 25).Take(2);
+            Print(result);
+        }
+
+    }
+}
