c7b85156b0b5df6d0d71c5906b958e26b25ea05d
LOMBARDIS-NB\LOMBARDIS <samuele.lombardi@nexusat.it>
2021-07-19 11:22:24 +0200

    Added ActionFilterAttribute project example

---
+++ ActionFilters/Filters/LogExceptionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionFilters.Filters
+{
+    public class LogExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            Debug.WriteLine("Logged error message: " + context.Exception.Message);
+        }
+    }
+}
---
+++ ActionFilters/Filters/ValidateRequestFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ActionFilters.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ActionFilters.Filters
+{
+    public class ValidateRequestFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                Debug.WriteLine("Model not valid !");
+                context.Result = new BadRequestResult();
+            }
+                
+
+
+        }
+    }
+
+
+}
