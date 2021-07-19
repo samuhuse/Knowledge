0be2be8126512b92c3c1e631cec93e17325de4d4
LOMBARDIS-NB\LOMBARDIS <samuele.lombardi@nexusat.it>
2021-07-19 11:17:32 +0200

    Added Yeld Return

---
+++ StructuralFeatures/YeldReturns.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralFeatures
+{
+    public class YeldReturns
+    {
+
+        [Test]
+        public void TryYeldMethod()
+        {
+            foreach (string item in GetFavouriteWords(new List<string>() { "hello", "word","my","name","is","samuele"}))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private IEnumerable<string> GetFavouriteWords(IEnumerable<string> words)
+        {
+            foreach (string word in words)
+            {
+                if ("hello".Equals(word, StringComparison.OrdinalIgnoreCase)) yield return word;
+                if ("word".Equals(word, StringComparison.OrdinalIgnoreCase)) yield return word;
+                if ("samuele".Equals(word, StringComparison.OrdinalIgnoreCase)) yield return word;
+            }
+        }
+
+    }
+}
